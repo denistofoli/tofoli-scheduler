f9e8a0b6ad1f88ae508302db3b68d7ee3489c4d0
Denis Tofoli <denis.tofoli@gmail.com>
2022-04-21 17:31:35 -0300

    fix: attribute result

--- tofoli_thread.py
+++ tofoli_thread.py
@@ -5,19 +5,19 @@ class tofoli_thread(threading.Thread):
     def __init__(self, cmd):
         threading.Thread.__init__(self)
         self.__cmd = cmd.split()
-        self.result = ''
+        self.__result = ''
 
 
     def run(self):
         try:
-            self.__std_out = subprocess \
+            self.__result = subprocess \
                 .run(self.__cmd, stdout=subprocess.PIPE) \
                 .stdout.decode('UTF-8') \
                 .__str__() \
                 .split('\n')
         except Exception as e:
-            self.__std_out = e.__str__()
+            self.__result = e.__str__()
 
 
     @property
-    def result(self): return self.result
+    def result(self): return self.__result
