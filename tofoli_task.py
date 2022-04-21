1e2d9127f29bb8c56fb793fe1a586827f6050e82
Denis Tofoli <denis.tofoli@gmail.com>
2022-04-21 17:08:41 -0300

    feat: Proporties

--- tofoli_task.py
+++ tofoli_task.py
@@ -13,10 +13,18 @@ class tofoli_task():
                  dows):
         self.__name = name
         self.__cmd = cmd
-        self.timer = tofoli_time_helper(start_date,
+        self.__timer = tofoli_time_helper(start_date,
                                         start_time,
                                         stop_time,
                                         group,
                                         interval_type,
                                         interval,
                                         dows)
+
+    @property
+    def cmd(self): return self.__cmd
+
+    @property
+    def get_next_run(self): return self.__timer.get_next_run
+
+    def next_run(self): self.__timer.next_run()
